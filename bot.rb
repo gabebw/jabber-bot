d6dcda2323a36ce084b0058bd8a6c777dc59a2df
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-07-29 18:30:57 -0400

    Respond to foodtruck inquiries.

--- bot.rb
+++ bot.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'bundler/setup'
 
 require 'easy-gtalk-bot'
@@ -8,5 +9,22 @@ Dir.glob('./lib/*.rb').each do |library|
   require library
 end
 
+# for foreman
+STDOUT.sync = true
+
 bot = GTalk::Bot.new(email: ENV['GOOGLE_EMAIL'], password: ENV['GOOGLE_PASSWORD'])
 bot.get_online
+
+bot.on_invitation do |inviter|
+  bot.message "Thanks for inviting me, #{inviter}. I'm accepting now!"
+  bot.accept_invitation(inviter)
+end
+
+bot.on_message do |from, text|
+  puts "#{from} says: #{text}"
+  if text =~ /foodtrucks/i
+    puts "#{from} is asking about foodtrucks"
+    bot.message "sure i can do foodtrucks"
+    bot.message Foodtruck.new.all
+  end
+end
